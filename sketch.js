f6197ea66c6f567bff396db790fcd95442acc623
sweathaprabhakar <70946320+sweathaprabhakar@users.noreply.github.com>
2020-10-31 17:56:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var ball;
+var database,position;
+
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var locNode=database.ref("Ball/Position");
+locNode.on("value",readposition);
+}
+
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref("Ball/Position").set({
+        x: position.x + x,
+        y: position.y + y
+    })
+}
+function readposition(data){
+    position=data.val();
+    ball.x=position.x;
+    ball.y=position.y;
+}
